9a130d4c851963bfaa76d463bad645a1c34ee317
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-05-29 14:47:39 +0530

    render components inside layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
-const Layout = () => {
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+<Head>
+  <title>Store</title>
+</Head>
+<header>
+  <Navbar/>
+</header>
+<main className='main-container'>
+  {children}
+</main>
+<footer>
+  <Footer/>
+</footer>
+    </div>
   )
 }
 
