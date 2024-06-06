1a165e94166aff338a9c457fcc55e4727f619ed0
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-06-06 15:15:11 +0530

    updated payment changes with buy-now onclick functionality and confetti visuals

--- pages/success.js
+++ pages/success.js
@@ -5,7 +5,7 @@ import { BsBagCheckFill } from 'react-icons/bs';
 import { useStateContext } from '../context/StateContext';
 import { runFireWorks } from '../lib/utils';
 
-const success = () => {
+const Success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
 
 useEffect(()=>{
@@ -39,4 +39,4 @@ runFireWorks();
     )
 }
 
-export default success
+export default Success
